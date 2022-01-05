87e047c59f4922691b230d0a3f98a8c4d6009b93
Li <mourisl@hotmail.com>
2022-01-05 00:03:12 -0500

    Add the script to convert combined vcf file to dat file.

---
+++ CombinedVcfToDat.pl
@@ -0,0 +1,230 @@
+#!/usr/bin/env perl
+
+use strict ;
+use warnings ;
+
+die "Usage: perl CombinedVcfToDat.pl reference.fa annotation.gtf combined.vcf > output.dat\n" if (@ARGV == 0) ;
+
+my %genome ;
+my %interestedGeneName ;
+
+my $i ;
+
+# Read in the reference genome.
+open FP1, $ARGV[0] ;
+my $chrom = "" ;
+my $seq = "" ;
+my $hasChrPrefix = 0 ;
+while ( <FP1> )
+{
+	if ( /^>/ )
+	{
+		$genome{ $chrom } = $seq if ( $chrom ne "" ) ;
+		$seq = "" ;
+		$chrom = substr( ( split )[0], 1 ) ;
+		if ( $chrom =~ /^c/ )
+		{
+			$hasChrPrefix = 1 ;
+		}
+	}
+	else
+	{
+		chomp ;
+		$seq .= $_ ;
+	}
+}
+$genome{ $chrom } = $seq if ( $chrom ne "" ) ;
+close FP1 ;
+
+# Read in the gene names from the combined vcf file
+open FP1, $ARGV[2] ;
+my %vcf ;
+while (<FP1>)
+{
+	next if (/^#/) ;
+	chomp ;
+	my @cols = split ;
+	$gene = (split /\*/, $cols[0])[0] ;
+	$interestedGeneName{$gene]}	= "." ;
+	push @{$vcf{$cols[0]}}, join("\t", @cols[1 .. $#cols ]) ;
+}
+close FP1 ;
+
+# Obtain the exon coordinates from the gtf file
+my %exons ;
+open FP1, $ARGV[1] ;
+my $prevTname = "-1" ;
+my $strand = "." ;
+while (<FP1>)
+{
+	next if ( /^#/ ) ;
+	chomp ;
+	my @cols = split /\t/ ;
+	next if ( $cols[2] ne "exon" ) ;
+
+	my $tname ;	
+	if ( $cols[8] =~ /transcript_name \"(.*?)\"/ )
+	{
+		#print $1, "\n" ; 
+		$tname = $1 ;
+	}
+	else
+	{
+		die "No transcript_name", $_, "\n" ;
+	}
+
+	if ( $tname ne $prevTname )
+	{
+		if ( (defined $interestedGeneName{ $gname } ) && 
+			&& $interestedGeneName{$gname} eq "." && @range > 0 )
+		{
+			$interestedGeneName{$gname}	= $strand ;
+			@{$exons{$gname}} = @range ;
+		}
+
+		$prevTname = $tname ;
+		if ( $cols[8] =~ /gene_name \"(.*?)\"/ )
+		{
+			#print $1, "\n" ; 
+			$gname = uc($1) ;
+		}
+		else
+		{
+			die "No gene_name: ", $_, "\n" ;
+		}
+		$strand = $cols[6] ;
+		undef @range ;
+	}
+	
+	if ( $hasChrPrefix == 1 && !( $cols[0] =~ /^c/) )
+	{
+		$cols[0] = "chr".$cols[0] ;
+	}
+	elsif ( $hasChrPrefix == 0 && $cols[0] =~ /^c/ )
+	{
+		$cols[0] = substr( $cols[0], 3 ) ;
+	}
+	push @range, $cols[0], $cols[3] - 1, $cols[4] - 1 ;			
+}
+close FP1 ;
+ 
+foreach my $allele (keys %vcf)
+{
+	my @alleleVcf = @{$vcf{$allele}} ;
+	my $gname = (split /\*/, $allele)[0] ;
+	my @alleleExon = @{$exons{$gname}} ;
+	my $seq = "" ;
+	my $padding = 500 ;
+	
+	my $chr = $alleleExon[0] ;
+	my $start = $alleleExon[1] - $padding ;
+	my $size = scalar(@alleleExon) ;
+	$start = 0 if ($start < 0) ;
+	my $end = $alleleExon[$size - 1] + $padding;
+	$end = $genome{$chr} - 1 if ($end >= length($genome{$chr})) ;
+	$seq = substr($genome{$chr}, $start, $end - $start + 1) ;
+	
+	my $offset = $start ;
+	my $firstOffset = $start ;
+	# Change the sequence according to the vcf.
+	foreach my $v (@alleleVcf)
+	{
+		my @cols = split /\t/, $v ;
+		my $pos = $cols[1] - 1 ;
+		if ($cols[3] ne '.' && $cols[4] ne '.')
+		{
+			substr($seq, $cols[3] - $offset, length($cols[3]), $cols[4]) ;
+			$offset += (length($cols[3]) - length($cols[4])) ;
+		}
+		elsif ($cols[3] eq '.' && $cols[4] ne '.') # insertion
+		{
+			substr($seq, $cols[3] - $offsest, 0, $cols[4]) ;
+			$offset -= length($cols[4]) ;
+		}
+		elsif ($cols[3] ne '.' && $cols[4] eq '.')
+		{
+			substr($seq, $cols[3] - $offsest, length($cols[3]), "") ;
+			$offset += length($cols[3]) ; 
+		}
+	}
+	# Move the exon coordinate according to the vcf file.
+	for ($i = 0 ; $i < scalar(@alleleExon) ; $i += 3)
+	{
+		$alleleExon[$i + 1] -= $firstOffset ;
+		$alleleExon[$i + 2] -= $firstOffset ;
+	}
+
+	foreach my $v (@alleleVcf)
+	{
+		my @cols = split /\t/, $v ;
+		my $pos = $cols[1] - 1 ;
+		my $shift = 0;
+		if ($cols[3] ne '.' && $cols[4] ne '.')
+		{
+			$shift = (length($cols[3]) - length($cols[4])) ;
+		}
+		elsif ($cols[3] eq '.' && $cols[4] ne '.')
+		{
+			$shift = length($cols[4]) ;
+		}
+		elsif ($cols[3] ne '.' && $cols[4] eq '.')
+		{
+			$shift = -length($cols[3]) ; 
+		}
+		else
+		{
+			next ;
+		}
+		
+		# exon coordinate should be reduced by 1 now.
+		for ($i = 0 ; $i < scalar(@alleleExon) ; $i += 3)
+		{
+			$alleleExon[$i + 1] += $shift if ($alleleExon[$i + 1] >= $pos); 
+			$alleleExon[$i + 2] += $shift if ($alleleExon[$i + 2] >= $pos);
+		}
+	}
+
+	# Reverse
+	$seq = uc($seq) ;
+	my $len = length($seq) ;
+	if ($interestedGeneName{$ganme} eq '-')
+	{
+		$seq = reverse($seq) ;
+		$seq =~ tr/ACGT/TGCA/ ;
+		my $j ;
+		for ($i = 0, $j = scalar(@alleleExon) - 3 ; $i < $j ; $i += 3, $j -= 3)
+		{
+			($alleleExon[$i + 1], $alleleExon[$j + 1]) = ($alleleExon[$j + 1], $alleleExon[$i + 1]) ;
+			($alleleExon[$i + 2], $alleleExon[$j + 2]) = ($alleleExon[$j + 2], $alleleExon[$i + 2]) ;
+		}
+		
+		for ($i = 0 ; $i < scalar(@alleleExon) ; $i += 3)
+		{
+			$alleleExon[$i + 1], $alleleExon[$i + 2]) = ($len - 1 - $alleleExon[$i + 2], 
+								$len - 1 - $alleleExon[$i + 1]) ;
+		}
+	}
+
+	# Output
+	print("ID   $allele\n")	;
+	print("FT   allele=$allele"\n) ;
+	if ($alleleExon[0] > 0)
+	{
+		print("FT   UTR            1..".$alleleExon[0]."\n") ;
+	}
+	for ($i = 0 ; $i < scalar(@alleleExon) ; $i += 3)
+	{
+		print("FT   exon          ".($alleleExon[$i + 1] + 1)."..".($alleleExon[$i + 2] + 1)."\n") ;
+		if ($i + 3 < scalar(@alleleExon)) 
+		{
+			print("FT   intron        ".($alleleExon[$i + 2] + 2)."..".($alleleExon[$i + 4])."\n") ;
+		}
+	}
+	if ($alleleExon[-1] < $len)
+	{
+		print("FT   UTR            ".($alleleExon[-1] + 2)."..".$len."\n") ;
+	}
+	print("SQ  Sequence $len BP\n") ;
+	print("$seq $len\n") ;
+	print("//") ;
+}
