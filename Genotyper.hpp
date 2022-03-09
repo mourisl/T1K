e40da41b2365442db62f49d66c1b7978d755ed34
Li <mourisl@hotmail.com>
2022-03-09 17:10:25 -0500

    Update weight function a little bit

--- Genotyper.hpp
+++ Genotyper.hpp
@@ -194,8 +194,8 @@ private:
 		//if (o.hasMatePair && o.overlap2.similarity < similarity)
 		//	similarity = o.overlap2.similarity ;
 		double segment = (1 - refSet.GetRefSeqSimilarity()) / 4.0 ;
-		if (segment < 0.02)
-			segment = 0.02 ;
+		if (segment < 0.01)
+			segment = 0.01;
 		if (similarity < 1 - 3 * segment)
 			ret = 0.01 ;
 		else if (similarity < 1 - 2 * segment)
